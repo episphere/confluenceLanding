9bf673c2b30f8b6e7805906c196c5866a57a3d59
lorenasandoval88 <43822820+lorenasandoval88@users.noreply.github.com>
2019-12-16 12:48:33 -0500

    Update dataRequest.js

--- src/pages/dataRequest.js
+++ src/pages/dataRequest.js
@@ -1,7 +1,7 @@
 export const template = () => {
     return `
         <div>
-            <h1><i class="fas fa-laptop-code"></i> Coming Soon...</h1>
+            <h1><i class="fas fa-laptop-code"></i> Coming Soon...Preview: https://www.youtube.com/watch?v=YtmpNfkvjdI</h1>
         </div>
     `;
 }
