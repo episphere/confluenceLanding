1ba29c1e911a8c5f50d95ed7c87dd83d4863ff92
lorenasandoval88 <43822820+lorenasandoval88@users.noreply.github.com>
2019-12-16 13:04:03 -0500

    Update dataRequest.js

--- src/pages/dataRequest.js
+++ src/pages/dataRequest.js
@@ -3,10 +3,14 @@ export const template = () => {
         <div>
             <h1><i class="fas fa-laptop-code"></i> Coming Soon...</h1>
             <script>
-            <a>Preview: https://www.youtube.com/watch?v=YtmpNfkvjdI
-            </a>var a = document.createElement('a');
-            var link = document.createTextNode("Analysis preview");
-            a.href = "https://www.geeksforgeeks.org";
+                
+                var an = document.createElement('an');
+                var link = document.createTextNode("Analysis preview");
+                a.appendChild(link);
+                a.title = "Analysis preview";
+                a.href = "https://www.youtube.com/watch?v=YtmpNfkvjdI";
+                document.body.appendChild(an);
+           </script> 
         </div>
     `;
 }
