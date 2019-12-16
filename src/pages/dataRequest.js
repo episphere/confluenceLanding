ef82fdff5e2bc3634cf89163eabe1eee275f676f
lorenasandoval88 <43822820+lorenasandoval88@users.noreply.github.com>
2019-12-16 12:56:08 -0500

    Update dataRequest.js

--- src/pages/dataRequest.js
+++ src/pages/dataRequest.js
@@ -1,7 +1,12 @@
 export const template = () => {
     return `
         <div>
-            <h1><i class="fas fa-laptop-code"></i> Coming Soon...Preview: https://www.youtube.com/watch?v=YtmpNfkvjdI</h1>
+            <h1><i class="fas fa-laptop-code"></i> Coming Soon...</h1>
+            <script>
+            <a>Preview: https://www.youtube.com/watch?v=YtmpNfkvjdI
+            </a>var a = document.createElement('a');
+            var link = document.createTextNode("Analysis preview");
+            a.href = "https://www.geeksforgeeks.org";
         </div>
     `;
 }
